b81b51a83825692c95d15380f5604b60d4803e43
leo_song <leo_songwei@126.com>
2015-09-10 18:22:50 +0800

    life game

---
+++ main.lisp
@@ -0,0 +1,116 @@
+(ql:quickload 'cl-ncurses)
+
+(defpackage game-of-life
+  (:use cl cl-user cl-ncurses))
+
+(in-package game-of-life)
+
+(defvar *width* 0)
+(defvar *height* 0)
+(defvar *screen-width* 0)
+(defvar *screen-height* 0)
+
+(defvar *array-cons* nil)
+
+(defun init-display ()
+  (initscr)
+  (raw)
+  (noecho)
+  (curs-set 0)
+  (clear)
+  (refresh)
+  (getmaxyx *stdscr*
+            *screen-height*
+            *screen-width*))
+
+(defun init-game ()
+  (setf *width* *screen-width*)
+  (setf *height* *screen-height*)
+
+  (setf *array-cons*
+        (cons (make-array (list *height* *width*))
+              (make-array (list *height* *width*))))
+  (init-random 3)
+  t)
+
+(defun init-random (rate)
+  "rate is a number between 0-10."
+  (let ((h (array-dimension (car *array-cons*) 0))
+        (w (array-dimension (car *array-cons*) 1)))
+    (dotimes (hi h)
+      (dotimes (wi w)
+        (if (< (random 10) rate)
+          (setf (aref (car *array-cons*) hi wi) 1)
+          (setf (aref (car *array-cons*) hi wi) 0))))))
+
+(defun put-dark (r c)
+  (attroff a_reverse)
+  (mvaddch r c (char-code #\Space)))
+
+(defun put-bright (r c)
+  (attron a_reverse)
+  (mvaddch r c (char-code #\Space)))
+
+(defun display-car ()
+  (dotimes (r *height*)
+    (dotimes (c *width*)
+      (let ((cell (aref (car *array-cons*) r c)))
+        (if (= 1 cell)
+          (put-bright r c)
+          (put-dark r c)))))
+  (refresh))
+
+
+(defun access-cell-square (r c)
+  (let ((y (mod r *height*))
+        (x (mod c *width*)))
+    (if (or (>= r *height*)
+            (>= c *width*)
+            (< r 0)
+            (< c 0))
+      0
+      (aref (car *array-cons*) y x))))
+
+(defun access-cell-loop (r c)
+  (let ((y (mod r *height*))
+        (x (mod c *width*)))
+    (aref (car *array-cons*) y x)))
+
+(defun access-cell (r c)
+  (let ((way #'access-cell-loop))
+    (funcall way r c)))
+
+(defun calculate-cell (r c)
+  (+ (access-cell (- r 1) (- c 1))
+     (access-cell (- r 1) (- c 0))
+     (access-cell (- r 1) (+ c 1))
+
+     (access-cell (- r 0) (- c 1))
+     (access-cell (- r 0) (+ c 1))
+
+     (access-cell (+ r 1) (- c 1))
+     (access-cell (+ r 1) (- c 0))
+     (access-cell (+ r 1) (+ c 1))))
+
+(defun between= (a x b)
+  (and (<= a x)
+       (<= x b)))
+
+(defun calculate-frame ()
+  (let ((a (car *array-cons*))
+        (d (cdr *array-cons*))
+        (h *height*)
+        (w *width*))
+    (dotimes (r h)
+      (dotimes (c w)
+        (let ((cell (aref a r c)))
+          (if (= 1 cell)
+            (setf (aref d r c)
+                  (if (between=
+                        2 (calculate-cell r c) 3)
+                    1 0))
+            (setf (aref d r c)
+                  (if (= 3 (calculate-cell r c))
+                    1 0))))))
+    (setf *array-cons*
+          (cons d a))))
