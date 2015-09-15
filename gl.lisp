bf03676d9b9b2e7e8f2522f85ca77c99dabfe88c
leo_song <leo_songwei@126.com>
2015-09-15 10:17:19 +0800

    GL

--- gl.lisp
+++ gl.lisp
@@ -5,26 +5,45 @@
   ()
   (:default-initargs
     :width 500 :height 500
-    :mode '(:single :rgb)
     :title "life game"))
+
+
+(defmethod glut:tick ((w game-window))
+  (calculate-frame)
+  (glut:post-redisplay))
 
 (defmethod glut:display-window
   :before ((w game-window))
+  (glut:enable-tick w 100)
   (gl:clear-color 0 0 0 0)
+  (gl:shade-model :smooth)
   (gl:matrix-mode :projection)
   (gl:load-identity)
   (glu:ortho-2d 0 500 500 0))
 
+(defun draw-square (r c)
+  (let ((len 3)
+        (left (* 4 c))
+        (top (* 4 r)))
+    (gl:with-primitive :polygon
+      (%gl:vertex-2i left top)
+      (%gl:vertex-2i (+ left len) top)
+      (%gl:vertex-2i (+ left len) (+ top len))
+      (%gl:vertex-2i left (+ top len)))))
+
+(defun gl-init ()
+  (let ((*screen-width* 125)
+        (*screen-height* 125))
+    (init-game))
+
 (defmethod glut:display ((w game-window))
   (gl:clear :color-buffer)
-  ;; Draw white polygon (rectangle) with corners at
-  ;; (0.25, 0.25, 0.0) and (0.75, 0.75, 0.0).
   (gl:color 0 1 0)
-  (gl:with-primitive :polygon
-    (%gl:vertex-2i 50 50)
-    (%gl:vertex-2i 450 50)
-    (%gl:vertex-2i 450 450)
-    (%gl:vertex-2i 50 450))
-  ;; Start processing buffered OpenGL routines.
-  (gl:flush))
+  (dotimes (r *height*)
+    (dotimes (c *width*)
+      (let ((cell (aref (car *array-cons*) r c)))
+        (if (= 1 cell)
+          (draw-square r c)
+          nil))))
+  (glut:swap-buffers))
 
